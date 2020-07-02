21bc5688508c3826bfc843208065fa5d08644bd3
Torens29 <44534255+Torens29@users.noreply.github.com>
2020-07-02 22:13:08 +0300

    доделать промисы. Добавить БД

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+let pagination = 1;
 
-const url = 'http://rostender.info/tender?p=1';
+const url = 'http://rostender.info/tender?p=';
 
-axios.get(url)
-    .then(response => {
 
-        let getData = html => {
-            data = [];
-            const $ = cheerio.load(html);
-            $('.tender-row').each((i, header) => {
-                
-                const title = $(header).find("a.description").attr('title');
-                const link = $(header).find("a.description").attr('href');
-                
-                
-                
-                
-                data.push({
-                    id:i,
-                    title: title.replace("\t",""),
-                    link : "http://rostender.info"+ link
-                });
-            });
-            console.log(data);
-        };
-        // fs.writeFile("hello.txt", response.data, function(error){
- 
-        //     if(error) throw error; // если возникла ошибка
-        //     console.log("Асинхронная запись файла завершена.");
-        // });
-
-        // console.log(response.data);
-        getData(response.data);
-    })
-    .catch(error => {
-        console.log(error);
+axios.get(url + pagination)
+    .then(response =>{
+        const $ = cheerio.load(response.data);
+        pagination = $('li.last a').attr('data-page');
+        console.log(pagination);
     });
+page=5
+for(page; page==1; page--){
+    axios.get(url + pagination)
+        .then(response => {
+            let getData = html => {
+                data = [];
+                const $ = cheerio.load(html);
+                $('.tender-row').each((i, header) => {
+                    
+                    const name = $(header).find("a.description").attr('title');
+                    const price =  $(header).find(".starting-price").text();
+                    const address = $(header).find('div.tender-address div').text();
+                    const dateStart = $(header).find('span.tender-date-info').text();
+                    const dateEnd = $(header).find('.tender-date-end').text();
+                    const link = $(header).find("a.description").attr('href');
+                    
+
+                    typeOfTender=[];
+                    for(j=0;j< $(header).find('.tender-costomer-branch a').length;j++){
+                        typeOfTender.push(
+                            $(header).find('.tender-costomer-branch a')[j].attribs.title
+                        );
+                    }
+
+                    data.push({
+                        id:i,
+                        name,
+                        price,
+                        address,
+                        dateStart,
+                        dateEnd,
+                        link : "http://rostender.info"+ link,
+                        typeOfTender
+                    });
+                });
+                console.log(data);
+            };
+            getData(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
